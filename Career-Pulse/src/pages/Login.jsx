4c3136ec913cbd940465c35fe081ff210e3e1dc2
N.B.P.Y.M. DE SILVA <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2024-03-17 23:16:32 +0530

    Set up backend folders and environment

--- Career-Pulse/src/pages/Login.jsx
+++ Career-Pulse/src/pages/Login.jsx
@@ -1,81 +1,44 @@
 import  { useState } from 'react';
 import './Signup_Login.css';
 import { Link } from 'react-router-dom';
-import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+// import axios from "axios";
+// import { useNavigate } from 'react-router-dom';
 import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [submittedWithoutCredentials, setSubmittedWithoutCredentials] = useState(false);
-  const navigate = useNavigate();
-  const [errors, setErrors] = useState({});
+  // const navigate = useNavigate();
   const  [showPassword, setShowPassword] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setErrors({ ...errors, [name]: '' }); // Clear validation errors on change
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    if (name === 'email' && value.trim() !== '') {
-      const emailError = !/\S+@\S+\.\S+/.test(value) ? 'Invalid email address' : '';
-      setErrors({ ...errors, [name]: emailError });
-    }
-
-    if (name === 'password') {
-      const passwordError = value.length < 8 ? '*Password needs at least 8 characters' : '';
-      setErrors({ ...errors, [name]: passwordError });
-    }
-  };
-
-  const handleSubmit = (e) => {
+  const loginUser = (e) => {
     e.preventDefault();
-
-    // Validation check before submitting
-    if (!email || !password || errors.email || errors.password) {
-      setSubmittedWithoutCredentials(true);
-      return;
-    }
-
-    axios.post('http://localhost:3000/login', { email, password })
-      .then(result => {
-        console.log(result)
-        if (result.data === "Success") {
-          navigate("/home");
-        } else {
-          // Handle invalid credentials
-          setErrors({ ...errors, credentials: '*Invalid email or password' });
-        }
-      })
-      .catch(err => console.log(err))
   };
 
   return (
     <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSubmit}>
+      <form className="signup-form" onSubmit={loginUser}>
         <h2>Login</h2>
 
         <label htmlFor="email">Email:</label>
         <input
           type="email"
-          id="email"
           name="email"
           placeholder='Enter email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          onBlur={handleChange}
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
         />
-        {errors.email && <p className="error">{errors.email}</p>}
 
         <label htmlFor="password">Password:</label>
         <div className='flex relative'>
         <input
-          id="password"
           type={showPassword ? "text":"password"}
           placeholder='Enter password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          onBlur={handleChange}
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
         />
         {showPassword ? (
               <FaEye 
@@ -91,11 +54,6 @@ function Login() {
             )}
         </div>
         <div>
-        {errors.password && <p className="error">{errors.password}</p>}
-        {errors.credentials && <p className="error">{errors.credentials}</p>}
-        {submittedWithoutCredentials && !email && !password && (
-          <p className="error">*Please enter your email and password</p>
-        )}
         </div>
 
           
