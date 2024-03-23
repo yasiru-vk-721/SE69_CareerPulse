ca6843bc98c7752f9941cb1eb3819ed5c7d7df58
YasiruVK <145313332+yasiru-vk@users.noreply.github.com>
2024-03-24 01:41:34 +0530

    update routes to add delete vacancy

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require('cors');
 
 
-const {test, registerUser, loginUser, getProfile, registerCompany, postJob,logOut,companyLogin, getCompanyProfile, getAllUsers, getVacancy}  = require('../controllers/authController.js'); //importing the test function from the authController
+const {test, registerUser, loginUser, getProfile, registerCompany, postJob,logOut,companyLogin, getCompanyProfile, getAllUsers, getVacancy, getPostedVacancy, deleteVacancy}  = require('../controllers/authController.js'); //importing the test function from the authController
 
 const { testMail } = require('../controllers/authMailController.js'); //importing the testMail function from the authMailController
 
@@ -21,12 +21,13 @@ router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/profile',getProfile)
 router.post('/company-register',registerCompany)
-router.get('/company-profile', getCompanyProfile)
+router.get('/company-profile', getCompanyProfile,getPostedVacancy)
 router.post('/posting', postJob)
 router.get('/logout',logOut)
 router.post('/company-login',companyLogin),
 router.get('/users', getAllUsers)
 router.get('/vacancy', getVacancy)
+router.delete('/vacancy/:id', deleteVacancy)
 router.post('/testMail', testMail)
 
 module.exports = router;
