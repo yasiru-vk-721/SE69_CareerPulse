192b6bd0c988be94c2df522a9607a1d8875c87dc
Mandeepa De Silva <131390063+Mandeepa-De-Silva@users.noreply.github.com>
2024-03-23 12:17:56 +0530

    Update vacancy.js

--- server/models/vacancy.js
+++ server/models/vacancy.js
@@ -2,27 +2,11 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const vacancySchema = new Schema({
-
-
-    companyName: String,
-
-    jobType: String,
-    jobRole: String,
-    location: String,
-    // email:{
-    //     type: String,
-    //     unique: true
-    // },
-    skills: String
-    // confirmpassword: String,
-    // jobStatus: String
-=======
+    companyName: String
     companyEmail: String,
     jobRole: String,
     jobType: String,
-    requirements: String,
-
-
+    requirements: String
 });
 
 const VacancyModle = mongoose.model('Vacancies', vacancySchema);
